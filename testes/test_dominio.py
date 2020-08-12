6854f1815c26747b312419aef5e64bd3134b4ff0
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2020-08-12 18:06:04 -0300

    Alterando arquivos da aula 2 do curso Testes automatizados: TDD com Python

--- testes/test_dominio.py
+++ testes/test_dominio.py
@@ -3,8 +3,8 @@ from dominio import Usuario, Lance, Leilao, Avaliador
 
 #Esta classe serve para relizar os testes nos metodos da classe Avaliador
 class TestAvaliador(TestCase):
-    #Este metodo serve para testar o metodo avalia da classe Avaliador
-    def test_avalia(self):
+    #Este metodo serve para testar a classe Avaliador passando a ela dois lances em ordem crescente e verificando o maior e o menor valor dos lances
+    def test_quando_passado_dois_lances_em_ordem_crescente_deve_retornar_o_maior_e_menor_valor_de_um_lance(self):
         # Criando os usuarios Fulano e Ciclano
         fulano = Usuario("Fulano")
         ciclano = Usuario("Ciclano")
@@ -31,5 +31,35 @@ class TestAvaliador(TestCase):
         #Comparando se os valores da instncia da classe Avaliador correspondem ao menor e o maior valor esperado
         self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
         self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
-    #Fim do metodo test_avalia
+    #Fim do metodo test_quando_passado_dois_lances_em_ordem_crescente_deve_retornar_o_maior_e_menor_valor_de_um_lance
+
+    #Este metodo serve para testar a classe Avaliador passando a ela dois lances em ordem decrescente e verificando o maior e o menor valor dos lances
+    def test_quando_passado_dois_lances_em_ordem_decrescente_deve_retornar_o_maior_e_menor_valor_de_um_lance(self):
+        # Criando os usuarios Fulano e Ciclano
+        fulano = Usuario("Fulano")
+        ciclano = Usuario("Ciclano")
+
+        # Crindo os lances de Fulano e Ciclano
+        lance_do_fulano = Lance(fulano, 500)
+        lance_do_ciclano = Lance(ciclano, 1500)
+
+        # Criando um leilao
+        leilao = Leilao("Celular")
+
+        # Adicionando os lances de Fulano e Ciclano ao leilao
+        leilao.lances.append(lance_do_ciclano)
+        leilao.lances.append(lance_do_fulano)
+
+        # Criando um avaliador
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        # Definindo o menor e o maior valor esperado
+        menor_valor_esperado = 500
+        maior_valor_esperado = 1500
+
+        # Comparando se os valores da instncia da classe Avaliador correspondem ao menor e o maior valor esperado
+        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
+        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+    # Fim do metodo test_quando_passado_dois_lances_em_ordem_decrescente_deve_retornar_o_maior_e_menor_valor_de_um_lance
 #Fim da classe TestAvaliador
